3d8f10b7337ea3e7ddd24e44f5957586d495ec68
Mojgan Ahmadi <mozhanahmadi@hotmail.com>
2017-12-05 11:25:18 -0500

    no message

--- mvc1/VidPlace/VidPlace/App_Start/RouteConfig.cs
+++ mvc1/VidPlace/VidPlace/App_Start/RouteConfig.cs
@@ -12,7 +12,22 @@ namespace VidPlace
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            
+            routes.MapMvcAttributeRoutes();
+
+
+            /*
+            //localhost:50913/media/released/2017/12
+            //in  constraints, we should insert 2 digits for month to be right
+            routes.MapRoute(
+             name: "media-released",
+             url: "media/released/{year}/{month}",
+             defaults: new { controller = "media", action = "released"},
+             constraints : new { year = @"\d{4}", month = @"^(0?[1-9]|1[012])$" }
+         );
+         */
+
+
+
 
             routes.MapRoute(
                name: "Default",
@@ -21,11 +36,16 @@ namespace VidPlace
            );
 
 
+
+
+
+         /*
             routes.MapRoute(
              name: "media-index",
              url: "media/index/{pageindex}/{sortby}",
              defaults: new { controller = "media", action = "index" }
          );
+         */
         }
     }
 }
--- mvc1/VidPlace/VidPlace/Controllers/MediaController.cs
+++ mvc1/VidPlace/VidPlace/Controllers/MediaController.cs
@@ -28,14 +28,14 @@ namespace VidPlace.Controllers
             // return HttpNotFound();
 
             /////////////////////////contoller-action(class-model)
-        
+
             return RedirectToAction("Customer", "Customer");
         }
 
 
 
 
-        public ActionResult edit(int id )
+        public ActionResult edit(int id)
         {
             //write this :
             ////////localhost:50913/media/edit/100
@@ -45,23 +45,37 @@ namespace VidPlace.Controllers
 
 
 
-       
 
-        public ActionResult index(int? pageindex , string sortby )
+
+        [Route("media/index/{ pageindex:range(1,100)}/{sortby maxlength(10)} ")]
+
+        public ActionResult index(int? pageindex, string sortby)
         {
-
-
-            //by default it shows me the ---pageindex=114 ---and  sortby = "Sort by Mojgan "
-            if (!pageindex.HasValue) pageindex = 114;
+             //by default it shows me the ---pageindex=114 ---and  sortby = "Sort by Mojgan "
+            if (!pageindex.HasValue) pageindex = 90;
             if (String.IsNullOrWhiteSpace(sortby)) sortby = "Sort by Mojgan ";
 
-
-
+            
             //but here i add  pageindex and sortby manually
             //write this :
             ////localhost:50913/media/index?pageindex=10&sortby=Date
 
-            return Content("pageindex =" + pageindex + "sortby ="    +  sortby);
+            return Content("pageindex =" + pageindex + "sortby =" + sortby);
+        }
+
+
+        
+
+
+
+        /////////////////////////url ==== media/released/2017/6
+         
+           [Route("media/released/{year}/{month} ")]
+
+        public ActionResult released(int year, int month)
+        {
+            
+            return Content("this year is  =" + year + "month =" + month);
         }
     }
 }
