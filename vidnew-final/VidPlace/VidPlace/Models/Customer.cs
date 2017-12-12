89764c1be0c8b741e3383574af8f8fcce047aa5d
Mojgan Ahmadi <mozhanahmadi@hotmail.com>
2017-12-12 13:17:11 -0500

    *vidnew--final project

---
+++ vidnew-final/VidPlace/VidPlace/Models/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace VidPlace.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Please enter the customer's name")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        //[MaxLength(255)]
+        [MaxLength(255,ErrorMessage ="Address cannot exceed 255 characters")]
+        public string Address { get; set; }
+
+        public Boolean IsSubscribedToNewsLetter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        [Display(Name ="Membership Type")]
+        public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [Min18YearsIfMemeber]
+        public DateTime? Birthdate { get; set; }
+
+        public List<Rental> Rentals { get; set; }
+
+        //override toString method
+        public override string ToString()
+        {
+            return "Customer name:" + Name + " | Address: " + Address;
+        }
+    }
+}
